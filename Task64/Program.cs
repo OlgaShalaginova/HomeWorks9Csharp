15e2ae4640174675820e7196f2f46886d6c3c98e
OlgaShalaginova <olechkatsapova@gmail.com>
2022-11-13 12:03:46 +0500

    Task64

---
+++ Task64/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задайте значение N. Напишите программу, которая выведет все чётные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+Console.Clear();
+
+Console.WriteLine("Введите натуральное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int a = 2;
+if (n%2 == 1) 
+{
+    n = n-1;
+}
+
+string NumbersRecMinus(int a, int b)
+{
+    if (a == b) return $"{a} ";
+    else return  NumbersRecMinus(a + 2, b) + $"{a} ";
+}
+
+Console.WriteLine(NumbersRecMinus(a, n));
